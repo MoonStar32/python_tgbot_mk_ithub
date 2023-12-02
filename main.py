9a9b7d1deb0a5f391e591cd4a6d2caf7689f43fc
Moon_Star32 <azamat.yusupov.04@outlook.com>
2023-12-02 14:29:39 +0300

    first_commit

---
+++ main.py
@@ -0,0 +1,37 @@
+# Библиотека 👇
+# pip install python-telegram-bot --pre
+
+# Погода    https://wttr.in/{message_text}
+# Википедия https://ru.wikipedia.org/wiki/{message_text}
+# Спорт     https://sport24.ru/search?text={message_text}
+
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
+
+async def start(update, context):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Напиши мне город, а я отправлю погоду в нём."
+    )
+
+async def send_data(update, context):
+    message_text = update.message.text
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f'Хочешь посмотреть в городе {message_text}?',
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton(text='Посмотреть...', web_app=WebAppInfo(f'https://wttr.in/{message_text}'))]]
+        )
+    )
+
+
+application = ApplicationBuilder().token('ВАШ ТОКЕН ИЗ BOTFATHER').build()
+
+start_handler = CommandHandler('start', start)
+application.add_handler(start_handler)
+
+weather_sender = MessageHandler(filters.TEXT, send_data)
+application.add_handler(weather_sender)
+
+application.run_polling()
